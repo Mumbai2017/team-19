4a3920ba0361fd663d405852885a1c821cdf9e87
akshay14ce1078 <deoghare.akshay.14ce1078@gmail.com>
2017-07-29 21:23:01 +0530

    inedx file changed abc and njo added

---
+++ pages/ngo.js
@@ -0,0 +1,6 @@
+callback({
+	"ngos":[{"name":"sakhi1","place":"lower parel","coordinates":{"lat":"18.993731","long":"72.825841"},"stock":"2"},
+		{"name":"sakhi2","place":"bandra","coordinates":{"lat":"19.0607","long":"72.8362"},"stock":"5"},
+		{"name":"sakhi3","place":"vikroli","coordinates":{"lat":"19.0912","long":"72.9209"},"stock":"3"}
+	]
+});
