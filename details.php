eb330ea4e0b2bb90cdceb8804c12f52319853d15
Abhi-geek <abhikateliya100@gmail.com>
2017-07-29 18:06:55 +0530

    customer UI
    customer UI pages and index.html file

---
+++ details.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Sa'Ni'Sa</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container" >
+  <h2>Customer Registration</h2>
+  <form class="form-horizontal" action="/action_page.php">
+      
+      <div class="form-group">
+      <label class="control-label col-sm-2" for="email">contact:</label>
+      <div class="col-sm-10">
+        <input type="number" class="form-control" id="phone_no" placeholder="Enter Phone" name="phone_no">
+      </div>
+    </div>
+      <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Name : </label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="c_name" placeholder="Enter name" name="c_name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Address:</label>
+      <div class="col-sm-10">          
+        <input type="textarea" class="form-control" id="c_address" placeholder="Enter address" name="c_address">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Landmark:</label>
+      <div class="col-sm-10">          
+        <input type="textarea" class="form-control" id="landmark" placeholder="Enter Landmark" name="landmark">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <a href="customer_details.php" type="submit" class="btn btn-default">Submit</a>
+      </div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
