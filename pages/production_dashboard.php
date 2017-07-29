661c400e1a87f48eb47ed3dfc10b9b3d0370fbaf
padmaja2023 <kollepadmaja23@gmail.com>
2017-07-29 21:58:36 +0530

    chartblack

--- pages/production_dashboard.php
+++ pages/production_dashboard.php
@@ -33,7 +33,7 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    
   
 </head>
 
@@ -163,7 +163,7 @@
                                 </div>
                             </div>
                         </div>
-                        <a href="#">
+                        <a href="blank.php">
                             <div class="panel-footer">
                                 <span class="pull-left">View Details</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -280,57 +280,9 @@
                             <div class="row">
                                  
                                 <div class="col-lg-12">
-                                    <div id="chart_div"></div>
-
-                                        <script>
-                                        google.charts.load('current', {packages: ['corechart', 'bar']});
-                                        google.charts.setOnLoadCallback(drawTopX);
-
-                                        function drawTopX() {
-                                              var data = new google.visualization.DataTable();
-                                              data.addColumn('timeofday', 'Time of Day');
-                                              data.addColumn('number', 'Motivation Level');
-                                             
-                                              data.addRows([
-                                                [{v: [8, 0, 0], f: '8 am'}, 1],
-                                                [{v: [9, 0, 0], f: '9 am'}, 2],
-                                                [{v: [10, 0, 0], f:'10 am'}, 3],
-                                                [{v: [11, 0, 0], f: '11 am'}, 4],
-                                                [{v: [12, 0, 0], f: '12 pm'}, 5],
-                                                [{v: [13, 0, 0], f: '1 pm'}, 6],
-                                                [{v: [14, 0, 0], f: '2 pm'}, 7],
-                                                [{v: [15, 0, 0], f: '3 pm'}, 8],
-                                                [{v: [16, 0, 0], f: '4 pm'}, 9],
-                                                ]);
-
-                                              var options = {
-                                                chart: {
-                                                  title: 'Motivation and Energy Level Throughout the Day',
-                                                  subtitle: 'Based on a scale of 1 to 10'
-                                                },
-                                                axes: {
-                                                  x: {
-                                                    0: {side: 'top'}
-                                                  }
-                                                },
-                                                hAxis: {
-                                                  title: 'Time of Day',
-                                                  format: 'h:mm a',
-                                                  viewWindow: {
-                                                    min: [7, 30, 0],
-                                                    max: [17, 30, 0]
-                                                  }
-                                                },
-                                                vAxis: {
-                                                  title: 'Rating (scale of 1-10)'
-                                                }
-                                              };
-
-                                              var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
-                                              materialChart.draw(data, options);
-                                            }                                    
-                                        </script>
-
+                                   <div id ="chartID"></div>
+                                    <script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>
+                                    <script type="text/javascript" src="../dist/js/barchart.js"></script>
                                 </div>
                                 <!-- /.col-lg-8 (nested) -->
                             </div>
