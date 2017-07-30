a19e6f16e13a773c96235e569e37ba2ba5d1ffcc
padmaja2023 <kollepadmaja23@gmail.com>
2017-07-30 08:56:03 +0530

    finalChanges

--- php/temp.php
+++ php/temp.php
@@ -1,27 +1,5 @@
-<?php
- $conn = new mysqli('localhost','root','','team_19');
- session_start();
-
-$phone = $_SESSION['phone'];
-// print_r($phone);
-//$quantity = $_POST['quantity'];
-//$pid = $_POST['p_id'];
-// $sql1 = "Select c_id from customer where phone_no = '$phone'";
-// $cid = $conn->query($sql1);
-// while ($row = $cid->fetch_assoc()) {
-//     echo $row['c_id'];
-
-// $cid1 = mysqli_fetch_assoc($cid);
-// echo $cid;
-// $sql2 = "Select * from product where p_id = '$pid'";
-// $run = $conn->query($sql2);
-// $run1 = (int)$run['p_cost'];
-// $total = $quantity * $run; 
-echo 'Order Placed';
-//$sql = "Insert into orders(c_id, p_id, quantity, status) values ('1', '$pid', '$quantity', 0)";
-
-//$conn->query($sql);
-
+<?php 
+      $conn = new mysqli('localhost','root','','sanisa_team19');
       $sql = "SELECT * FROM customer WHERE c_id=1";
       $result = $conn->query($sql);
       if($result->num_rows >0){
@@ -54,9 +32,8 @@ echo 'Order Placed';
             
         
       }
-// }
-?>
-
+      
+      ?>
 
 <!DOCTYPE html>
 <html>
@@ -66,25 +43,22 @@ echo 'Order Placed';
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
-<body onload="loadMap();">
-  <div id="viewMin">
-    
+<body>
+  <div id="right-panel">
+    <input type="number" name="index" id="index">
   </div>
   <script>
       
     //created as Global just to debug in console
-	
     var source = "<?php echo $c_address?>";
     //var destination= 
     var results;
-    function loadMap() {
-		console.log("error getting location");
+    $("#distance").click(function () {
       //getting Geolocation from browser
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
           var geocoder = new google.maps.Geocoder;
           var service = new google.maps.DistanceMatrixService;
-		  dest= ["<?php echo $column?>"];
           //getting distance from Google
           service.getDistanceMatrix({
             origins: [source],
@@ -99,15 +73,14 @@ echo 'Order Placed';
             } else {
               var originList = response.originAddresses;
               var destinationList = response.destinationAddresses;
-             // var outputDiv = document.getElementById('output');
+              var outputDiv = document.getElementById('output');
                 var min=999,index=0;
-				var loc;
-              //outputDiv.innerHTML = '';
+              outputDiv.innerHTML = '';
                 var results;
               for (var i = 0; i < originList.length; i++) {
                 results = response.rows[i].elements;
                 for (var j = 0; j < results.length; j++) {
-                  //outputDiv.innerHTML += results[j].distance.text;
+                  outputDiv.innerHTML += results[j].distance.text;
                     if(min>parseInt(results[j].distance.text)){
                         min=results[j].distance.text;
                         index=j;
@@ -116,9 +89,8 @@ echo 'Order Placed';
                 }
               }
         console.log("Distance : "+min+", index : "+index);
-		
-            var txtbox = document.getElementById("viewMin");
-                txtbox.innerHTML=min+" , "+dest[index];
+            var txtbox = document.getElementById("index");
+                txtbox.innerHTML=index;
             }
           });
         }, function () {
@@ -126,7 +98,7 @@ echo 'Order Placed';
         });
       }
         
-    }
+    });
   </script>
   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMxdGmGa60mA7EV-iPYauPrj9XB35qA6Q">
   </script>
